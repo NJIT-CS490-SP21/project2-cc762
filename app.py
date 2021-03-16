f60e9937376d8d8fd47f3245635fea62182d3144
EC2 Default User <ec2-user@ip-172-31-14-74.ec2.internal>
2021-03-16 23:01:56 +0000

    Yapf run

--- app.py
+++ app.py
@@ -21,63 +21,63 @@ db.session.commit()
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    json=json,
-    manage_session=False
-)
+socketio = SocketIO(app,
+                    cors_allowed_origins="*",
+                    json=json,
+                    manage_session=False)
 
 #Segment for handling the users
 users = []
 x = None
 o = None
 #server side turn var
-turn = 0;
+turn = 0
 board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+
 
 def checkWin():
     global board
     global turn
-    
+
     lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-    ];
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
     for i in lines:
-        a = i[0];
-        b = i[1];
-        c = i[2];
-        if board[a] ==  board[b] and board[b] == board[c] and board[c] in "XO":
-            return board[a];
-    return False;
+        a = i[0]
+        b = i[1]
+        c = i[2]
+        if board[a] == board[b] and board[b] == board[c] and board[c] in "XO":
+            return board[a]
+    return False
 
 
 def addUser(user):
     global x, o
     from models import Player
-    if(len(users) == 0):
+    if (len(users) == 0):
         tile = "X"
         x = user
-    elif(len(users) == 1):
+    elif (len(users) == 1):
         tile = "O"
         o = user
     else:
         tile = "Spectator"
     newUser = {"name": user, "xo": tile}
     users.append(newUser)
-    if(Player.query.filter_by(username=user).first() == None):
+    if (Player.query.filter_by(username=user).first() == None):
         db.session.add(Player(username=user, points=100, wins=0, losses=0))
         db.session.commit()
     leader = getLeaderBoard()
     socketio.emit('leaderboard', leader, broadcast=True, include_self=True)
     return newUser
+
 
 def getLeaderBoard():
     from models import Player
@@ -85,8 +85,14 @@ def getLeaderBoard():
     leader = []
     for p in query:
         print(p)
-        leader.append( {"username":p.username, "points":p.points, "wins":p.wins, "losses":p.losses} )
+        leader.append({
+            "username": p.username,
+            "points": p.points,
+            "wins": p.wins,
+            "losses": p.losses
+        })
     return leader
+
 
 def applyWinner(win):
     from models import Player
@@ -112,45 +118,69 @@ def applyWinner(win):
     leader = getLeaderBoard()
     socketio.emit('leaderboard', leader, broadcast=True, include_self=True)
 
+
 @app.route('/', defaults={"filename": "index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
 
+
 @socketio.on("requestUserList")
 def on_requestUserList(data):
     print("User List Requested")
-    socketio.emit('requestUserList', users, to=data["id"], broadcast=False, include_self=True)#note the false broadcast, because we dont want everyone to update anytime someone asks for the list
+    socketio.emit(
+        'requestUserList',
+        users,
+        to=data["id"],
+        broadcast=False,
+        include_self=True
+    )  #note the false broadcast, because we dont want everyone to update anytime someone asks for the list
+
 
 @socketio.on("leaderboard")
 def on_requestLeader(data):
     from models import Player
     print("Leaderboard Requested")
     leader = getLeaderBoard()
-    socketio.emit('leaderboard', leader, to=data["id"], broadcast=False, include_self=True)
+    socketio.emit('leaderboard',
+                  leader,
+                  to=data["id"],
+                  broadcast=False,
+                  include_self=True)
+
 
 # When a client connects from this Socket connection, this function is run
 @socketio.on('login')
 def on_login(data):
     print('User login!')
-    if "usr" in data.keys():#proper error handilng for empty/misformed packets not implemented
+    if "usr" in data.keys(
+    ):  #proper error handilng for empty/misformed packets not implemented
         user = addUser(data["usr"])
     print(data["id"])
-    socketio.emit('addUserCallback', user, to=data["id"], broadcast=False, include_self=True)
-    socketio.emit('updateUsers', data, broadcast=True, include_self=False)#Tells all instances a new user has been added
-    
+    socketio.emit('addUserCallback',
+                  user,
+                  to=data["id"],
+                  broadcast=False,
+                  include_self=True)
+    socketio.emit(
+        'updateUsers', data, broadcast=True,
+        include_self=False)  #Tells all instances a new user has been added
+
+
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
 def on_disconnect():
     print('User disconnected!')
-    
+
+
 @socketio.on('connect')
 def on_connect():
     print('User connected!')
 
+
 # When a client emits the event 'board' to the server, this function is run
 @socketio.on('board')
-def on_chat(data):
+def on_board_update(data):
     from models import Player
     global turn, board
     turn += 1
@@ -164,26 +194,29 @@ def on_chat(data):
         print("winner signal emitted")
     if turn >= 9:
         socketio.emit('win', "Draw", broadcast=True, include_self=True)
-    data["turn"] = turn;
+    data["turn"] = turn
     print("Emmitting Board Change")
     print(data)
     socketio.emit('board', data, broadcast=True, include_self=True)
+    return board
+
 
 #Reset board
 @socketio.on('reset')
 def on_reset():
     print("Resetting Game")
     global board, users, turn, x, o
-    board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"] 
+    board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
     users = []
     turn = 0
     x = None
     o = None
     socketio.emit('reset', broadcast=True, include_self=True)
 
+
 # Note we need to add this line so we can import app in the python shell
 if __name__ == "__main__":
-# Note that we don't call app.run anymore. We call socketio.run with app arg
+    # Note that we don't call app.run anymore. We call socketio.run with app arg
     socketio.run(
         app,
         host=os.getenv('IP', '0.0.0.0'),
